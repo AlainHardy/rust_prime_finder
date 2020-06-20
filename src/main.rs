2b2787143a54ce685c9b2b292299a8e7368b7ec4
Alain Hardy <alain.hardy@protonmail.com>
2020-06-20 18:37:08 +0200

    Take argument to get the upper limit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use std::time::{Instant};
+use std::env;
 
 mod prime_finder;
 
 fn main() {
-  let upper_limit = 100;
+  let args: Vec<String> = env::args().collect();
+
+  match args.len() {
+    1 => { println!("You must provide at least a positive integer."); return; }
+    _ => {}
+  }
+
+  let upper_limit = match args[1].parse() {
+    Ok(u) => u,
+    Err(x) => {
+      println!("Failed to parse parameter: {}", x);
+      return;
+    }
+  };
+
   let start_time = Instant::now();
   let prime_list = prime_finder::compute_for(upper_limit);
   let time_spent = start_time.elapsed();
